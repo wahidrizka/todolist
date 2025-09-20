4529373efc7aceb5a2d73bfd2631f2597d39393f
wahidrizka <wahid.0310@students.amikom.ac.id>
2025-09-21 03:37:31 +0700

    fix(version): perjelas deskripsi endpoint versi (tampilkan metadata build)

--- src/main/java/com/example/todolist/api/VersionController.java
+++ src/main/java/com/example/todolist/api/VersionController.java
@@ -18,7 +18,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/version")
-@Tag(name = "Version", description = "Informasi versi aplikasi")
+@Tag(name = "Version", description = "Informasi versi & metadata build")
 public class VersionController {
 
   private final BuildProperties build;
